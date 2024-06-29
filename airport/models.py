8420cb108119220c15e3fd1512826868581fcbbd
Eugene <eugenekozhenevskiy@gmail.com>
2024-06-29 13:56:10 +0300

    add docker & refactor models

--- airport/models.py
+++ airport/models.py
@@ -1,14 +1,34 @@
-from django.db import models
+import os
+import uuid
+from django.core.exceptions import ValidationError
+from django.utils.text import slugify
 from django.conf import settings
+from django.db import models
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+def movie_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+    dirname = f"{slugify(type(instance).__name__)}s/"
+
+    return os.path.join("uploads/", dirname, filename)
 
 
 class Airplane(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
     airplane_type = models.ForeignKey(
-        "AirplaneType", on_delete=models.CASCADE, related_name="airplanes"
+        AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
     )
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
 
     @property
     def capacity(self) -> int:
@@ -18,16 +38,9 @@ class Airplane(models.Model):
         return self.name
 
 
-class AirplaneType(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Crew(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
 
     @property
     def full_name(self):
@@ -35,31 +48,46 @@ class Crew(models.Model):
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class City(models.Model):
-    name = models.CharField(max_length=100)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE, related_name="cities"
+    )
+
+    class Meta:
+        verbose_name = "city"
+        verbose_name_plural = "cities"
 
     def __str__(self):
-        return f"Country: {self.country}, City: {self.name}"
+        return f"Country: {self.country}(city: {self.name})"
 
 
 class Airport(models.Model):
-    name = models.CharField(max_length=100)
-    closet_big_city = models.ForeignKey(City, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    closest_big_city = models.ForeignKey(
+        City, on_delete=models.CASCADE, related_name="airports"
+    )
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
 
     def __str__(self):
-        return f"{self.name} ({self.closet_big_city})"
+        return f"{self.name} ({self.closest_big_city})"
+
+    class Meta:
+        ordering = ["name"]
 
 
 class Route(models.Model):
     source = models.ForeignKey(
-        Airport, on_delete=models.CASCADE, related_name="sources_route"
+        Airport, on_delete=models.CASCADE, related_name="routes_source"
     )
     destination = models.ForeignKey(
-        Airport, on_delete=models.CASCADE, related_name="destinations_route"
+        Airport, on_delete=models.CASCADE, related_name="routes_destination"
     )
     distance = models.IntegerField()
 
@@ -67,34 +95,89 @@ class Route(models.Model):
         return f"{self.source} - {self.destination}"
 
 
-class Order(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        Airplane, on_delete=models.CASCADE, related_name="flights"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew)
 
     def __str__(self):
-        return f"{self.created_at} - {self.user}"
+        return f"{self.route}: departure {self.departure_time}, arrival {self.arrival_time}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
 
 
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
     flight = models.ForeignKey(
-        "Flight", on_delete=models.CASCADE, related_name="flights"
+        Flight, on_delete=models.CASCADE, related_name="tickets"
     )
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="tickets"
+    )
+
+    @staticmethod
+    def validate_ticket(row, seat, airplane, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, airplane_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(airplane, airplane_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name:
+                            f"{ticket_attr_name} "
+                            f"number must be in available range: "
+                            f"(1, {airplane_attr_name}): "
+                            f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.flight.airplane,
+            ValidationError,
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    class Meta:
+        unique_together = ("flight", "row", "seat")
+        ordering = ["row", "seat"]
 
     def __str__(self):
-        return f"Order: {self.order} (row: {self.row}, seat: {self.seat})"
-
-
-class Flight(models.Model):
-    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name="routes")
-    airplane = models.ForeignKey(
-        Airplane, on_delete=models.CASCADE, related_name="airplanes"
-    )
-    departure_time = models.DateTimeField()
-    arrival_time = models.DateTimeField()
-    crew = models.ForeignKey(Crew, on_delete=models.CASCADE, related_name="crews")
-
-    def __str__(self):
-        return self.route
+        return (
+            f"{str(self.flight)} (row: {self.row}, seat: {self.seat})"
+        )
