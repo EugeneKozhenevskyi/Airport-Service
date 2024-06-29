f0d0c872d39b685d82e0098b44c5b5c81d87029d
Eugene <eugenekozhenevskiy@gmail.com>
2024-06-29 19:32:15 +0300

    fix

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=0))
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
